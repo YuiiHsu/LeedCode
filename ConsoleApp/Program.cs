2d461db83b147c33f6fe7c84ffb0864d8b527870
Yui <yuiiidevelop@gmail.com>
2019-09-17 16:28:30 +0800

    Search Insert Position
    Runtime: 100 ms, faster than 60.23% of C# online submissions for Search Insert Position.
    Memory Usage: 23.9 MB, less than 5.26% of C# online submissions for Search Insert Position.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] a = new int[4] { 1, 3, 5, 6 };
+            Console.WriteLine(p.SearchInsert(a,2));
+            Console.Read();
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int index = Array.IndexOf(nums, target);
+
+            if (index == -1)
+            {
+                Array.Resize(ref nums, nums.Length + 1);
+                nums[nums.Length -1 ] = target;
+                Array.Sort(nums);
+                index = Array.IndexOf(nums, target);
+            }
+
+            return index;
+        }
+    }
+}
